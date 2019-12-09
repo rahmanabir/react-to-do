e13926f2a1bc7d877bdac52805c0427cc8d52be4
rahmanabir <abir.rahman1995@gmail.com>
2019-12-09 17:33:56 +0600

    To Do list features added

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import Header from './components/layout/Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React the hard way
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Finish assignment',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Eat breakfast',
+        completed: false
+      },
+      {
+        id: 4,
+        title: 'Do something',
+        completed: false
+      }
+    ]
+  }
+
+  //Toggle Complete
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed  // ! acts as a toggle  
+        }
+
+        return todo;
+      })
+    })
+
+  }
+
+  //Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+
+  }
+
+
+  render() {
+
+    return (
+      <div className="App">
+
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        </div>
+
+      </div>
+
+
+    );
+  }
 }
 
 export default App;
